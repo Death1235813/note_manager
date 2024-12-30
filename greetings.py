d5fc93b42a90d1d3b931d2d0e59d89e1a6908120
Death1235813 <wdgsptaa@gmail.com>
2024-12-30 19:52:22 +0700

    Add files via upload

---
+++ greetings.py
@@ -0,0 +1,12 @@
+username = 'Death'
+title = 'Grade 1'
+content = 'Начало работы над проектом'
+status = 'В разработке'
+created_date = '30-12-2024'
+issue_date = '30-01-2025'
+print('Имя пользователя:',username)
+print('Заголовок заметки:',title)
+print('Описание заметки:',content )
+print('Статус заметки:',status)
+print('Дата создания заметки:',created_date)
+print('Дата истечения заметки:',issue_date)
