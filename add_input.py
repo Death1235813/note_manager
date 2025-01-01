2e5faa293f550cdeda4ea3b97df4e67fe0334e5e
Death1235813 <wdgsptaa@gmail.com>
2025-01-01 23:23:32 +0700

    Add files via upload

---
+++ add_input.py
@@ -0,0 +1,15 @@
+from datetime import datetime
+from datetime import timedelta
+username = input('Имя пользователя: ')
+title = input('Заголовок заметки: ')
+content = input('Описание заметки: ')
+status = input('Статус заметки: ')
+due_date = int(input('Срок выполнения (в днях): '))
+created_date = datetime.now()
+issue_date = datetime.now() + timedelta(days = due_date)
+print('Имя пользователя:',username)
+print('Заголовок заметки:',title)
+print('Описание заметки:',content )
+print('Статус заметки:',status)
+print('Дата создания заметки: {}-{}'.format(created_date.day,created_date.month))
+print('Дата истечения заметки: {}-{}'.format(issue_date.day,issue_date.month))
