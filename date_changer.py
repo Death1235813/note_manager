4a28d8d12dfaad901c756a4025b1a3e68d9705dd
Death1235813 <wdgsptaa@gmail.com>
2025-01-03 16:23:22 +0700

    date_changer.py

--- date_changer.py
+++ date_changer.py
@@ -4,8 +4,9 @@ username = 'Death'
 title = 'Начало проекта'
 content = 'Второе задание проекта'
 status = 'В разработке'
+due_date = int(input('Срок выполнения заметки (в днях): '))
 created_date = datetime.now()
-issue_date = datetime.now() + timedelta(days = 30)
+issue_date = datetime.now() + timedelta(days = due_date)
 print('Имя пользователя:',username)
 print('Заголовок заметки:',title)
 print('Описание заметки:',content )
