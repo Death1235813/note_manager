e06fe8fe71d0724e172a1f562b566c1aadf932e0
Death1235813 <wdgsptaa@gmail.com>
2025-01-10 15:28:37 +0700

    final.py

--- final.py
+++ final.py
@@ -1,14 +1,19 @@
 from datetime import datetime
 from datetime import timedelta
+username = input('Имя пользователя: ')
+title1, title2, title3 = input('Первый заголовок заметки: '), input('Второй заголовок заметки: '), input('Третий заголовок заметки: ')
+titles = [title1, title2, title3]
+content = input('Описание заметки: ')
+status = input('Статус заметки: ')
 due_date = int(input('Срок выполнения заметки (в днях): '))
 created_date = datetime.now()
 issue_date = datetime.now() + timedelta(days = due_date)
 note = [
-input('Имя пользователя: '),
-input('Описание заметки: '),
-input('Статус заметки: '),
+username,
+content,
+status,
 str(created_date.day) + '-' + str(created_date.month),
 str(issue_date.day) + '-' + str(issue_date.month),
-[input('Первый заголовок заметки: '), input('Второй заголовок заметки: '), input('Третий заголовок заметки: ')]
+titles
 ]
 print(note)
